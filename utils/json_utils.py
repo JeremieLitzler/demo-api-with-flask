476129831ddfbff04b4213d4bf792b69517ea266
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-04-19 16:32:33 +0200

    add toJSON source of inspiration

--- utils/json_utils.py
+++ utils/json_utils.py
@@ -1,5 +1,8 @@
 import json
 
+# See https://stackoverflow.com/questions/3768895/how-to-make-a-class-json-serializable?noredirect=1&lq=1
+# Below is the first answer
+
 def toJSON(obj):
     return json.dumps(
         obj,
