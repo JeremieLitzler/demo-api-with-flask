23cf68061a9a6019a25dfcf59243448cb9a5633a
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-05-14 14:27:11 +0200

    update routes to following the best practices (https://restfulapi.net/resource-naming/)

--- src/controllers/api_project.py
+++ src/controllers/api_project.py
@@ -6,7 +6,7 @@ from services.service_project import *
 from dto.ProjectDto import ProjectDto
 
 
-@app.route("/api/v1.0/project", methods=["POST"])
+@app.route("/api/v1.0/projects", methods=["POST"])
 def api_project_add():
     data = request.get_json()
     if not data:
@@ -16,19 +16,19 @@ def api_project_add():
     return response
 
 
-@app.route("/api/v1.0/project/all", methods=["GET"])
+@app.route("/api/v1.0/projects", methods=["GET"])
 def api_project_get_all():
     projects = get_all()
     return projects
 
 
-@app.route("/api/v1.0/project/<string:id>", methods=["GET"])
+@app.route("/api/v1.0/projects/<string:id>", methods=["GET"])
 def api_project_get(id: int):
     response = get_one(id)
     return response
 
 
-@app.route("/api/v1.0/project/<string:id>", methods=["PUT"])
+@app.route("/api/v1.0/projects/<string:id>", methods=["PUT"])
 def api_project_update(id):
     # Logic to update a specific project by ID
     data = request.get_json()
@@ -39,7 +39,7 @@ def api_project_update(id):
     return response
 
 
-@app.route("/api/v1.0/project/<string:id>", methods=["DELETE"])
+@app.route("/api/v1.0/projects/<string:id>", methods=["DELETE"])
 def api_project_delete(id):
     response = delete(id)
     return response
