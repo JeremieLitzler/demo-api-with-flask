f05ae1e60b0fcb2c5705b2e2ee49d9062b4cde69
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-05-14 15:15:04 +0200

    remove extra lines in service_project

--- src/services/service_project.py
+++ src/services/service_project.py
@@ -10,13 +10,9 @@ import json
 from sqlalchemy.orm import scoped_session
 
 from app import app
-
 from utils.api_utils import get_response_json
-
 from dto.ProjectDto import ProjectDto
-
 from dao.models import Project
-
 import dal.dal_project as dal_project
 
 
@@ -61,115 +57,69 @@ def add(project_data: ProjectDto) -> None:
         # Return True if at least one row was added
 
         message = "null" if True else "No record affected"
-
         return get_response_json(new_project.id, True, message)
-
     except Exception as ex:
-
         print(ex)
-
         return get_response_json(id, False, ex)
-
     finally:
-
         print("finished calling create_project")
 
 
 def get_one(id: str, noJson=False) -> ProjectDto:
-
     try:
-
         project = dal_project.fetch_one(id)
-
         if noJson:
             return project
 
         if project == None:
-
             return get_response_json(id, False, "Project not found", 404)
         else:
-
             return jsonify(project)
-
     except Exception as ex:
-
         print(ex)
-
         return get_response_json(id, False, ex, 500)
-
     finally:
-
         print("finished calling get_project")
 
 
 def get_all() -> list[ProjectDto]:
-
     try:
-
         projects = dal_project.fetch_all()
-
         return jsonify(projects)
-
     except Exception as ex:
-
         print(ex)
-
         return get_response_json(id, False, ex)
-
     finally:
-
         print("finished calling get_projects")
 
 
 def update(project_data: ProjectDto) -> None:
-
     try:
-
         project = dal_project.fetch_one(project_data.id)
-
         if project == None:
-
             return get_response_json(id, False, "Project not found", 404)
 
         if project_data.name is not None:
-
             project.name = project_data.name
-
         if project_data.color is not None:
-
             project.color = project_data.color
 
         dal_project.save()
-
         return jsonify(project)
-
     except Exception as ex:
-
         print(ex)
-
         return get_response_json(id, False, ex)
-
     finally:
-
         print("finished calling update_project")
 
 
 def delete(id: str) -> bool:
-
     try:
-
         result = dal_project.delete(id)
-
         message = "Project deleted successfully" if result else "No record affected"
-
         return get_response_json(id, True, message, 204)
-
     except Exception as ex:
-
         print(ex)
-
         return get_response_json(id, False, ex)
-
     finally:
-
         print("finished calling delete_project")
